598187926ba404101fe4d2a0cf8a26f1a0aa0bb5
Telmo Valverde <telmo.valverde@tarmac.io>
2022-06-14 20:57:14 -0300

    Recording 'previous_position'

---
+++ src/game_state.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var previous_position = Vector3(0,0,0)
+
+func _ready():
+  pass # Replace with function body.
--- src/player/player.gd
+++ src/player/player.gd
@@ -21,8 +21,13 @@ func die():
     if translation: 
         translation = last_floor_position
 
-func _on_LastPositionTimer_timeout():
-    if is_on_floor() && get_last_slide_collision().collider is StaticBody:
-            last_floor_position = translation
+#func _on_LastPositionTimer_timeout():
+#    if is_on_floor() && get_last_slide_collision().collider is StaticBody:
+#            last_floor_position = translation
+#
+#    last_position_timer.start()
 
-    last_position_timer.start()
+# Record previous position so orbs can follow player.
+func _on_Timer_timeout():
+  GameState.previous_position = global_transform.origin
+  print("previous_position = %s" % GameState.previous_position)
