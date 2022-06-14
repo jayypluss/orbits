c199a3fd53ed45a1e8ef56d57208d516ea95b016
Jonathan F. K <jonathanfk1@gmail.com>
2022-06-14 11:00:59 -0300

    Adding Player.

---
+++ src/player/player.gd
@@ -0,0 +1,28 @@
+tool
+class_name Player
+extends KinematicBody
+# Helper class for the Player scene's scripts to be able to have access to the
+# camera and its orientation.
+
+onready var camera: CameraRig = $CameraRig
+#onready var skin: Mannequiny = $Mannequiny
+onready var state_machine: StateMachine = $StateMachine
+onready var last_position_timer = $LastPositionTimer
+
+var last_floor_position: Vector3
+
+func _ready():
+    last_position_timer.start()
+
+func _get_configuration_warning() -> String:
+    return "Missing camera node" if not camera else ""
+
+func die():
+    if translation: 
+        translation = last_floor_position
+
+func _on_LastPositionTimer_timeout():
+    if is_on_floor() && get_last_slide_collision().collider is StaticBody:
+            last_floor_position = translation
+
+    last_position_timer.start()
